31f8db339aa5518ca84958d6d9051cd700636df3
Ronald <60875145+relfering@users.noreply.github.com>
2021-10-03 21:20:10 +0200

    Update Win10-Pro-20H1-ztiwest.ps1

--- Win10-Pro-20H1-ztiwest.ps1
+++ Win10-Pro-20H1-ztiwest.ps1
@@ -26,8 +26,8 @@ Import-Module OSD -Force
 
 Write-Host -ForegroundColor Green "Start OSDCloud"
 
-#Start-OSDCloud -OSLanguage nl-nl -OSBuild 20H1 -OSEdition Pro -ZTI
-Start-OSDCloud -OSBuild 21H1 -OSEdition Pro -OSLanguage nl-nl -OSLicense Volume -ZTI
+Start-OSDCloud -OSLanguage nl-nl -OSBuild 21H1 -OSEdition Pro -ZTI
+#Start-OSDCloud -OSBuild 21H1 -OSEdition Pro -OSLanguage nl-nl -OSLicense Volume -ZTI
 
 #Restart from WinPE
 
