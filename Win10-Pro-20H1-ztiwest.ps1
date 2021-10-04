5e9e23b5e98e769c9548790196cfd87c5c15756c
Ronald <60875145+relfering@users.noreply.github.com>
2021-10-04 21:16:03 +0200

    Update Win10-Pro-20H1-ztiwest.ps1

--- Win10-Pro-20H1-ztiwest.ps1
+++ Win10-Pro-20H1-ztiwest.ps1
@@ -1,38 +1,34 @@
-Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
-
+Write-Host  -ForegroundColor Cyan "Starting SeguraOSD's Custom OSDCloud ..."
 Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
-
 if ((Get-MyComputerModel) -match 'Virtual') {
-
-Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
-
-Set-DisRes 1600
-
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
 }
 
 #Make sure I have the latest OSD Content
-
-Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-
+Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
 Install-Module OSD -Force
 
-Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
-
+Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
 Import-Module OSD -Force
 
+#TODO: Spend the time to write a function to do this and put it here
+Write-Host  -ForegroundColor Cyan "Ejecting ISO"
+Write-Warning "That didn't work because I haven't coded it yet!"
+#Start-Sleep -Seconds 5
+
 #Start OSDCloud ZTI the RIGHT way
-
-Write-Host -ForegroundColor Green "Start OSDCloud"
-
+Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
 Start-OSDCloud -OSLanguage nl-nl -OSBuild 21H1 -OSEdition Enterprise -ZTI
-#Start-OSDCloud -OSBuild 21H1 -OSEdition Pro -OSLanguage nl-nl -OSLicense Volume -ZTI
+#Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+
+#Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
+Write-Warning "I'm not sure of what to put here yet"
 
 #Restart from WinPE
-
-Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
-
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
-
 wpeutil reboot
