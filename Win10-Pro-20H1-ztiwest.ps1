a027a4757f03571118d3f90c8eb0dea71dedd903
Ronald <60875145+relfering@users.noreply.github.com>
2021-10-04 21:41:34 +0200

    Update Win10-Pro-20H1-ztiwest.ps1

--- Win10-Pro-20H1-ztiwest.ps1
+++ Win10-Pro-20H1-ztiwest.ps1
@@ -1,4 +1,4 @@
-Write-Host  -ForegroundColor Cyan "Starting SeguraOSD's Custom OSDCloud ..."
+Write-Host  -ForegroundColor Cyan "Starting Omroep West OSD's Custom OSDCloud ..."
 Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
@@ -8,10 +8,10 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 }
 
 #Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
+Write-Host  -ForegroundColor Cyan "Updating the OSD PowerShell Module"
 Install-Module OSD -Force
 
-Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Write-Host  -ForegroundColor Cyan "Importing the OSD PowerShell Module"
 Import-Module OSD -Force
 
 #TODO: Spend the time to write a function to do this and put it here
@@ -20,13 +20,13 @@ Write-Warning "That didn't work because I haven't coded it yet!"
 #Start-Sleep -Seconds 5
 
 #Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
+Write-Host  -ForegroundColor Cyan "Start OSDCloud with Omroep West parameters"
 Start-OSDCloud -OSLanguage nl-nl -OSBuild 21H1 -OSEdition Enterprise -ZTI
 #Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
-Write-Warning "I'm not sure of what to put here yet"
+Write-Warning "One moment ..."
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
