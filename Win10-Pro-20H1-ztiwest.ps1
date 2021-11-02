4bb4de3bb78268506e9a39174eba6a9ba5399f5c
Ronald <60875145+relfering@users.noreply.github.com>
2021-11-02 16:39:41 +0100

    Update Win10-Pro-20H1-ztiwest.ps1

--- Win10-Pro-20H1-ztiwest.ps1
+++ Win10-Pro-20H1-ztiwest.ps1
@@ -15,8 +15,8 @@ Write-Host  -ForegroundColor Cyan "Importing the OSD PowerShell Module"
 Import-Module OSD -Force
 
 #TODO: Spend the time to write a function to do this and put it here
-Write-Host  -ForegroundColor Cyan "Ejecting ISO"
-Write-Warning "One moment ..."
+#Write-Host  -ForegroundColor Cyan "Ejecting ISO"
+#Write-Warning "One moment ..."
 #Start-Sleep -Seconds 5
 
 #Start OSDCloud ZTI the RIGHT way
