0145838016ad99c8455b364ad7f3524bdc60d702
Ronald <60875145+relfering@users.noreply.github.com>
2021-12-21 12:08:48 +0100

    Update Win11-LT-ztiwest.ps1

--- Win11-LT-ztiwest.ps1
+++ Win11-LT-ztiwest.ps1
@@ -21,8 +21,8 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with West bedrijfs parameters ..... "
-Start-OSDCloudGUI
-#Start-OSDCloud -OSLanguage nl-nl -OSBuild 21H2 -OSEdition PRO -ZTI
+#Start-OSDCloudGUI
+Start-OSDCloud -OSLanguage nl-nl -OSBuild 21H2 -OSEdition PRO -ZTI
 #Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
@@ -34,6 +34,6 @@ Start-OSDCloudGUI
 #Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel Edit -Confirm:$false
 
 #Restart from WinPE
-#  Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-#  Start-Sleep -Seconds 20
-#  wpeutil reboot
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
